f15d75b1ced5f5e433fd10abea70a0055237434a
Edyta <42374858+jarzedyt@users.noreply.github.com>
2019-12-14 15:50:49 +0100

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,41 +1,18 @@
-
 const promoSlider = document.querySelector("#promo__slider");
 const sliderList = document.querySelector("#slider__list");
 const sliderItems = document.querySelectorAll(".slider__item");
+const backButton = document.querySelector("#back-button");
+const nextButton = document.querySelector("#next-button");
 
 let width = promoSlider.offsetWidth;
 const animationSpeed = 2000;
 const pause = 3000;
 let currentSlide = 1; //2,3,4,5,6 (bo 6 slide to 1 obrazek, któy jest podmieniany po kryjomu na 1 slide potem)
 
-let interval;
-
-const sliderStart = () => {
-  interval = setInterval(() => {
-    /*sliderList.animate(
-      { "margin-left": "-=" + width },
-      animationSpeed,
-      () => {
-        currentSlide++;
-        if (currentSlide === (sliderList.length - 2)) {
-          currentSlide = 1;
-          sliderList.css("margin-left", 0);
-        }
-      }
-    );*/
-    
-    //sliderList.forEach(item => {
-      console.log("."); 
-      //zrób tak, że dla każdej wartości currentSlide
+//zrób tak, że dla każdej wartości currentSlide
       //margin listy jest przenoszony na currentSlide * width - ale płynnie
       //chyba że to przejście z 5 na 1 to wtedy podmianka
       //niech slide1 będzie 1 i ostatnim slidem xd
-    //});
-  }, pause);
-};
-
-const sliderStop = () => {
-  clearInterval(interval);
-};
-
-sliderStart();
+nextButton.addEventListener('click', event => {
+  sliderList.style["margin-left"] = `-${width}`;
+});
