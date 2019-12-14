b4de1e43c1f34f3f1f9debbea9224b40f5c589bd
Edyta <jarzebska.edyta@gmail.com>
2019-12-14 11:56:08 +0100

    slider cd

--- slider.js
+++ slider.js
@@ -1,19 +1,18 @@
 
+const promoSlider = document.querySelector("#promo__slider");
+const sliderList = document.querySelector("#slider__list");
+const sliderItems = document.querySelectorAll(".slider__item");
 
-const width = 1192;
+let width = promoSlider.offsetWidth;
 const animationSpeed = 2000;
 const pause = 3000;
-let currentSlide = 1;
-
-const promoSlider = document.querySelector("#promo__slider");
-const sliderList = document.querySelectorAll(".slider_list");
-const sliderItem = document.querySelectorAll(".slider__item");
+let currentSlide = 1; //2,3,4,5,6 (bo 6 slide to 1 obrazek, któy jest podmieniany po kryjomu na 1 slide potem)
 
 let interval;
 
 const sliderStart = () => {
   interval = setInterval(() => {
-    sliderList.animate(
+    /*sliderList.animate(
       { "margin-left": "-=" + width },
       animationSpeed,
       () => {
@@ -23,7 +22,15 @@ const sliderStart = () => {
           sliderList.css("margin-left", 0);
         }
       }
-    );
+    );*/
+    
+    //sliderList.forEach(item => {
+      console.log("."); 
+      //zrób tak, że dla każdej wartości currentSlide
+      //margin listy jest przenoszony na currentSlide * width - ale płynnie
+      //chyba że to przejście z 5 na 1 to wtedy podmianka
+      //niech slide1 będzie 1 i ostatnim slidem xd
+    //});
   }, pause);
 };
 
